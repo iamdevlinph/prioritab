4ac695fbf15b39f5cc97c84cadea50bbdf9edaea
iamdevlinph <iamdevlinph@gmail.com>
2017-10-12 18:39:25 +0800

    Done UI To Do: Sync for both quick links and tasks CRUD quick links

---
+++ prioritab-bundle/lib/iamdevlinph.js
@@ -0,0 +1,11 @@
+$(function () {
+  var
+    $quickLinksToggle = $('#quick-links--toggle--btn'),
+    $quickLinksList = $('#quick-links--list');
+
+  $quickLinksToggle.click(function () {
+    $quickLinksList.toggle("slide", {
+      direction: "left"
+    }, 1000);
+  });
+});
